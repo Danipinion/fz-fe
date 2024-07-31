f4035b7d22d41da8557363ed20b301e892679e93
naufinur <khayatinaufi23gmail.com>
2024-07-31 12:40:30 +0700

    ⚙:fix fetch

--- src/instance.js
+++ src/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3003",
-  // baseURL: "https://sz-be.vercel.app/",
+  // baseURL: "http://localhost:3003",
+  baseURL: "https://sz-be.vercel.app/",
 });
 
 export default instance;
